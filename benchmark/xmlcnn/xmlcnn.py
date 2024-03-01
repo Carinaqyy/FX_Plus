5ea3254549973466ed1d3227c0d02eeb48c995cd
Carinaqyy <yuying_quan@ucsb.edu>
2024-03-01 09:14:51 +0000

    Clean up xmlcnn code

--- benchmark/xmlcnn/xmlcnn.py
+++ benchmark/xmlcnn/xmlcnn.py
@@ -1,17 +1,28 @@
+# Automatically generated file. Do not modify!
 
-from xmlcnn_impl import XMLCNN as XMLCNNImpl
-from fx_plus.helper import BaseTestCase, UnitTestBase, emptyAttributeCls
-import argparse
-
+import os
 import json
 import torch
+from fx_plus.helper import BaseTestCase, emptyAttributeCls
+import argparse
+from xmlcnn_impl import XMLCNN as XMLCNNImpl
+from fx_plus.compiler.passes import LossEliminate
+from fx_plus.compiler.passes import FrontendPass
+from fx_plus.compiler.passes import DecompositionPass
+from fx_plus.compiler.passes import LocalCSE
+from fx_plus.compiler.passes import Reassociation
+from fx_plus.compiler.passes import TritonFusionPass
 
+
+# Model frontends and testbenchs
 
 class XMLCNN(XMLCNNImpl):
     """
     The XML-CNN comes from the paper Deep Learning for Extreme Multi-label Text
     Classification with dynamic pooling
     """        
+    name = "XMLCNN"
+    dtype = torch.float16
         
     def __init__(self, config_json: str):
         # config_json: path to the json file containing the model configuration
@@ -31,14 +42,13 @@ class XMLCNN(XMLCNNImpl):
         
     def get_sample_inputs(self):
         # generate the example inputs for profiling and verification
-
-        # Input embedding
+        #Input embedding
         e_emb = torch.randn(
-            size=(self.batch_size, self.sequence_length, self.embedding_dim), dtype=torch.float, device="cuda"
+            size=(self.batch_size, self.sequence_length, self.embedding_dim), dtype=torch.float16, device="cuda"
         )
-        # Binary labels
+        #         #Binary labels
         y = torch.randint(
-            size=(self.batch_size, self.y_dim), dtype=torch.float, device="cuda", low=0, high=2
+            size=(self.batch_size, self.y_dim), dtype=torch.float16, device="cuda", low=0, high=2
         )
         return e_emb, y
         
@@ -95,18 +105,48 @@ class XMLCNN(XMLCNNImpl):
         self.batch_size = config_dict["batch_size"]
         return self.batch_size
 
-class XMLCNN_Profile(BaseTestCase):
+
+class XMLCNNTB(BaseTestCase):
     """
-    Profile and verify the XMLCNN model
+    Testbed of the XMLCNN model
     """
     cls = XMLCNN
 
-if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description="XMLCNN End-to-End Training with CUDA Graph")
-    parser.add_argument('--json_path', '-f', type=str, required=False, help="Path to json file")
-    args = parser.parse_args()
 
-    ###########################################################################
-    profiler = XMLCNN_Profile(args.json_path)
-    profiler(verify=True)
+if __name__ == '__main__':
+    script_dir = os.path.dirname(os.path.abspath(__file__))
+    default_json_file = os.path.join(script_dir, "xmlcnn.json")
+    parser = argparse.ArgumentParser(
+        description="XMLCNN End-to-End Training with CUDA Graph")
+    parser.add_argument(
+        '--json_path', '-f', type=str, default=default_json_file, 
+        help="Path to json file")
+    parser.add_argument(
+        '--verify', '-v', action='store_true', 
+        help="verify the results against the reference")
+    parser.add_argument(
+        '--profile', '-p', action='store_true',
+        help="profile the models"
+    )
+    parser.add_argument(
+        '--visualize', '-s', action='store_true',
+        help="visualize the dataflow graph before and after the transformation"
+    )
+    args = parser.parse_args()
+    
+    passes = []
+    passes.append(LossEliminate())
+    passes.append(FrontendPass())
+    passes.append(DecompositionPass())
+    passes.append(LocalCSE())
+    passes.append(Reassociation())
+    passes.append(TritonFusionPass())
+
+    
+    XMLCNNTB(args.json_path)(
+        verify=args.verify, 
+        profile=args.profile, 
+        visualize=args.visualize,
+        passes=passes)
+
 
